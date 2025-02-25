934ad09862b5253e5f30b9d2407fde16b2ca9dd0
PreetPant <pant.bprit23@gmail.com>
2025-02-25 12:59:34 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,35 @@
+//if
+// const isUserloggedIn=true
+// const temperature=41
+
+// if(temperature<50){
+// console.log("less than 50");
+// }
+// else{
+// console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+
+
+// const score=200
+// if(score >100){
+//     let power="fly"
+//     // console.log(`User power:${power}`);
+    
+// }
+// console.log(`User power:${power}`);
+
+
+// 
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+
+if(userLoggedIn && debitCard && 2==3){
+console.log("allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail){
+console.log("user logged in");
+}
