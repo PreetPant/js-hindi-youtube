e0332cdde6628d5edb902e1147885d1a5f3568ca
PreetPant <pant.bprit23@gmail.com>
2025-07-27 19:13:47 +0530

    Spring Boot Journal app
    By Vipul Tyagi

---
+++ journalApp/journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -0,0 +1,33 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection="users")
+@Data
+public class User {
+    @Id
+    private ObjectId id;
+    @Indexed(unique=true)
+    @NonNull
+    private String userName;
+    @NonNull
+    private String password;
+
+    //for db link and user journal communication
+    @DBRef
+    private List<JournalEntry> journalEntries=new ArrayList<>();
+
+    //for spring security  //user ke pas kya kya role hai
+    private List<String> roles;
+
+}
